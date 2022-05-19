c6f7839920b074fe4fb46c51a1caf67be6973a04
B-Bala8055 <8055balalee@gmail.com>
2022-05-19 19:43:07 +0530

    refer code:03

--- embedded/bare.py
+++ embedded/bare.py
@@ -11,56 +11,75 @@ def vote():
     print('*Vote casting Section*')
     print('')
 
+    # Fetch all political parties in a list
     parties = fetchAllParty()
 
-    print('Select Party')
-
+    # Checking for errors
     if parties == 0:
         print('Error when fetching parties')
         print('')
         time.sleep(3)
         fun['main_menu']()
 
+    # If no errors, come here
     else:
+        # Print all political parties
+        print('Select Party')
+        print('')
+
         for i in range(len(parties)):
             print(f'{i+1}. {parties[i]}')
+        print('')
 
+        # Entering choice
         choice = input('Choice? : ')
         print('')
 
-        selection = parties[(int(choice)-1)]
-        print(f'Selected {selection}')
-        print('')
-        imgPath = 'D:\\FINAL YR PROJ\\ECEfinalproject\\embedded\\fps\\999000.bmp'
-        status = castVote(selection, imgPath)
-
-        print('*')
-        print('Fingerprint is grabbed from localstorage instead of sensor')
-        print('D:\FINAL YR PROJ\ECEfinalproject\embedded\fps\999000.bmp')
-        print('*')
-        print('')
-        print('Processing... Please Wait')
-        print('')
-        if status == 200 or '200':  # 200 means success
-            print('Vote Casted')
-            print('')
-            time.sleep(3)
-            fun['main_menu']()
+        # Checking if choice is valid
+        if(int(choice) > len(parties)):
+            fun['invalid']()
+        # If valid, come here
         else:
-            print('Vote Not casted. Please contact admin')
+            # Printing choice
+            selection = parties[(int(choice)-1)]
+            print(f'Selected {selection}')
             print('')
-            time.sleep(3)
-            fun['main_menu']()
+
+            imgPath = 'D:\\FINAL YR PROJ\\ECEfinalproject\\embedded\\fps\\999000.bmp'
+            # Casting vote which returns http status code
+            status = castVote(selection, imgPath)
+
+            print('*')
+            print('Fingerprint is grabbed from localstorage instead of sensor')
+            print('D:\FINAL YR PROJ\ECEfinalproject\embedded\fps\999000.bmp')
+            print('*')
+            print('')
+            print('Processing... Please Wait')
+            print('')
+
+            if status == 200 or '200':  # 200 means success
+                print('Vote Casted')
+                print('')
+                time.sleep(3)
+                fun['main_menu']()
+            else:
+                print('Vote Not casted. Please contact admin')
+                print('')
+                time.sleep(3)
+                fun['main_menu']()
 
 
 def register():
     global aadhar
     print('*Register fingerprint Section*')
     print('')
+
+    # Aadhar input
     aadhar = input('Enter your aadhar : ').strip()
     print('')
 
     imgPath = 'D:\\FINAL YR PROJ\\ECEfinalproject\\embedded\\fps\\999000.bmp'
+    # Regiser voter returns http status code
     status = registerVoter(aadhar, imgPath)
 
     print('*')
@@ -69,7 +88,7 @@ def register():
     print('*')
     print('')
 
-    if(status == 201 or 201):
+    if(status == 201 or 201):  # 201 means successful
         print('Regn successful')
         print('')
         time.sleep(3)
@@ -81,24 +100,41 @@ def register():
         fun['main_menu']()
 
 
+def invalid():
+    print('Invalid Input')
+    print('')
+    time.sleep(3)
+    fun['main_menu']()
+
+
 def main_menu():
     global choice
+    # Print choices
     print('1. Vote')
     print('2. Register')
     print('')
+
+    # Choice input and decision
     choice = input('Choice? : ')
     if(choice == '1'):
         fun['vote']()
     elif(choice == '2'):
         fun['register']()
     else:
-        print('Nothing')
+        fun['invalid']()
 
 
-fun = {'vote': vote, 'register': register, 'main_menu': main_menu}
+# This is a function mapper.(Functions are put in a dictionary as values)
+# How to use
+# Striucture fun['vote']()
+# fun['key'] - gives the value which is a function
+# () at the end calls the function
+fun = {'vote': vote, 'register': register,
+       'invalid': invalid, 'main_menu': main_menu}
 
 
 def main():
+    # Show main menu first
     main_menu()
 
 
